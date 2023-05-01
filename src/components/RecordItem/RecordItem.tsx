7381654a4b11bcde40378f41296f372c5e509b70
emanuelalvaradog <emanuel.alvarado.g@gmail.com>
2023-05-01 17:07:49 -0600

    fix: change svg bug

--- src/components/RecordItem/RecordItem.tsx
+++ src/components/RecordItem/RecordItem.tsx
@@ -37,7 +37,7 @@ export function RecordItem({
         <div className={styles.itemFocus}>
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            enable-background="new 0 0 32 32"
+            enableBackground="new 0 0 32 32"
             viewBox="0 0 32 32"
             id="edit"
           >
