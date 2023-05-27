bb49eb0e9ce2ebb69ccc19eb4f732afc3cb286bc
emanuelalvaradog <emanuel.alvarado.g@gmail.com>
2023-05-27 16:24:11 -0600

    styles: change waitlist page responsiveness

--- src/pages/waitlist.tsx
+++ src/pages/waitlist.tsx
@@ -1,7 +1,39 @@
+import Head from "next/head";
+import Link from "next/link";
+import styles from "@/styles/Waitlist.module.scss";
+
 export default function Waitlist() {
   return (
     <div>
-      <h1>Waitlist</h1>
+      <Head>
+        <title>Naptic App - Chat with your data</title>
+        <meta name="description" content="Naptic App - Chat with your data" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <header className={styles.header}>
+        <Link href="/" className={styles.headerLogo}>
+          <h1>naptic</h1>
+        </Link>
+        <Link href="/login" className={styles.headerCTA}>
+          Comenzar ahora
+        </Link>
+      </header>
+
+      <main className={styles.main}>
+        <div className={styles.mainContent}>
+          <h1>¡Reserva tu lugar!</h1>
+          <h1>
+            <span>Pronto serás parte de la revolución</span>
+          </h1>
+          <p>
+            Estamos muy emocionados de compartir contigo nuestro próximo
+            lanzamiento. ¡En estos momentos ya estás en la lista de espera!
+            Estamos liberando acceso poco a poco. Pronto recibirás acceso a los
+            nuevos features de naptic.
+          </p>
+        </div>
+      </main>
     </div>
   );
 }
