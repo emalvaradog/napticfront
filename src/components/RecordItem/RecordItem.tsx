4ba9e7f61b2d919cbf513cd43c91e5ad524dd23c
emanuelalvaradog <emanuel.alvarado.g@gmail.com>
2023-05-27 16:40:40 -0600

    styles: add styling to selected record item

--- src/components/RecordItem/RecordItem.tsx
+++ src/components/RecordItem/RecordItem.tsx
@@ -1,22 +1,36 @@
 import { useState } from "react";
 import styles from "./styles.module.scss";
+import { useSelector } from "react-redux";
+import { RootState } from "@/store/store";
+import { WorkSpaceScreen } from "@/interfaces/WorkSpaceInterfaces";
 
 export function RecordItem({
   title,
   date,
   onClick,
   onDelete,
+  id,
 }: {
   title: string;
   date: string;
+  id: string;
   onClick: () => void;
   onDelete: (e: React.MouseEvent<HTMLDivElement>) => void;
 }) {
-  const [titleInput, setTitleInput] = useState(title.trim());
-  const [isEditing, setIsEditing] = useState(false);
+  const { selectedRecord } = useSelector((state: RootState) => state.records);
+  const { currentScreen } = useSelector((state: RootState) => state.auth);
 
   return (
-    <button className={styles.item} onClick={onClick}>
+    <button
+      className={`${styles.item} ${
+        selectedRecord?.id === id &&
+        // @ts-ignore
+        currentScreen === WorkSpaceScreen.SelectedRecord
+          ? styles.itemSelected
+          : ""
+      }`}
+      onClick={onClick}
+    >
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" id="sound">
         <path d="M2 24.5c.82813 0 1.5-.67139 1.5-1.5V9c0-.82861-.67188-1.5-1.5-1.5S.5 8.17139.5 9v14C.5 23.82861 1.17188 24.5 2 24.5zM9.09961 28.40918V3.59082c0-.82861-.67188-1.5-1.5-1.5s-1.5.67139-1.5 1.5v24.81836c0 .82861.67188 1.5 1.5 1.5S9.09961 29.23779 9.09961 28.40918zM14.7002 23V9c0-.82861-.67188-1.5-1.5-1.5s-1.5.67139-1.5 1.5v14c0 .82861.67188 1.5 1.5 1.5S14.7002 23.82861 14.7002 23zM20.2998 28.40918V3.59082c0-.82861-.67188-1.5-1.5-1.5s-1.5.67139-1.5 1.5v24.81836c0 .82861.67188 1.5 1.5 1.5S20.2998 29.23779 20.2998 28.40918zM25.90039 23V9c0-.82861-.67188-1.5-1.5-1.5s-1.5.67139-1.5 1.5v14c0 .82861.67188 1.5 1.5 1.5S25.90039 23.82861 25.90039 23zM30 2.09082c-.82813 0-1.5.67139-1.5 1.5v24.81836c0 .82861.67188 1.5 1.5 1.5s1.5-.67139 1.5-1.5V3.59082C31.5 2.76221 30.82813 2.09082 30 2.09082z"></path>
       </svg>
